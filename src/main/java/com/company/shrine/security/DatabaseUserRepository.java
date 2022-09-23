c7b624ff0e19ee7fc3925638dc3053f74ffb788f
Yerlen <yerlen.toktarov@gmail.com>
2022-09-23 11:30:46 +0600

    Initial commit

---
+++ src/main/java/com/company/shrine/security/DatabaseUserRepository.java
@@ -0,0 +1,31 @@
+package com.company.shrine.security;
+
+import com.company.shrine.entity.User;
+import io.jmix.securitydata.user.AbstractDatabaseUserRepository;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Primary
+@Component("miko_UserRepository")
+public class DatabaseUserRepository extends AbstractDatabaseUserRepository<User> {
+
+    @Override
+    protected Class<User> getUserClass() {
+        return User.class;
+    }
+
+    @Override
+    protected void initSystemUser(User systemUser) {
+        Collection<GrantedAuthority> authorities = getGrantedAuthoritiesBuilder()
+                .addResourceRole(FullAccessRole.CODE)
+                .build();
+        systemUser.setAuthorities(authorities);
+    }
+
+    @Override
+    protected void initAnonymousUser(User anonymousUser) {
+    }
+}
---
+++ src/test/java/com/company/shrine/ShrineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.shrine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShrineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
